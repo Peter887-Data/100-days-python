9b3fb8e0393b120cdcce4d52c99cd982fc9f5800
Thomas Zub <thomas.zub@outlook.de>
2023-04-17 21:16:43 +0200

    Add example for imported module without if guard

---
+++ module_main/module_with.py
@@ -0,0 +1,8 @@
+import module_without
+
+
+def main_with():
+  print("main with")
+
+if __name__ == "__main__":
+  main_with()
---
+++ module_main/module_without.py
@@ -0,0 +1,7 @@
+import module_with
+
+
+def main_without():
+  print("main without")
+
+main_without()
