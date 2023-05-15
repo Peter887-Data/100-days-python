42b72795def531db872a7796de0d0e20d387379a
Peter887-Data <laubinio@gmail.com>
2023-05-15 20:40:45 +0200

    Day6: create hangman game, incl. finding each equal letter

--- Day6/Hangman.py
+++ Day6/Hangman.py
@@ -1,7 +1,3 @@
-word = "thomas"
-guessed_word = "_"*len(word)
-
-
 def input_letter():
     letter = input("Enter Letter: ")
     if len(letter) != 1:
@@ -10,23 +6,26 @@ def input_letter():
     return letter
 
 
-hangman_count = 0
+if __name__ == "__main__":
+    word = "peer"
+    guessed_word = "_"*len(word)
+    hangman_count = 0
 
-
-while True:
-    letter = input_letter()
-    if letter in word:
-        # ToDo: add while condition to replace all equal letters in word
-        pos = word.find(letter)
-        guessed_word = guessed_word[:pos] + letter + guessed_word[pos +1:]
-        print(guessed_word)
-        if guessed_word == word:
-            print("won")
-            break
-    else:
-        hangman_count += 1
-        print(hangman_count)
-        if hangman_count >= 10:
-            print("Game Over")
-            break
+    while True:
+        letter = input_letter()
+        if letter in word:
+            pos = word.find(letter)
+            while pos >= 0:
+                guessed_word = guessed_word[:pos] + letter + guessed_word[pos +1:]
+                pos = word.find(letter, pos+1)
+            print(guessed_word)
+            if guessed_word == word:
+                print("won")
+                break
+        else:
+            hangman_count += 1
+            print(hangman_count)
+            if hangman_count >= 10:
+                print("Game Over")
+                break
 
